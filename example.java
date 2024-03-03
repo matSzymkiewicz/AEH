e8e139a28f665ff5421ecc353b223e0e87565711
matSzymkiewicz <161924739+matSzymkiewicz@users.noreply.github.com>
2024-03-03 08:47:24 +0100

    test

--- example.java
+++ example.java
@@ -1,5 +1,11 @@
 public class example {
 public static void main(string[] args) {
     System.out.printIn("Hello World");
+    int x = 3 
+    int y = 2 
+    int suma = x + y 
+    //dodawanie 
+    system.out.printIn("Wynik:" +suma
+    )
 }
 }
